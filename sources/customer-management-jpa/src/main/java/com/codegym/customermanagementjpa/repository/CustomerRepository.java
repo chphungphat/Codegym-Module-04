a81ad9fa14c7196e28a7d0efab2b97fbc4a0adeb
chphungphat <chphungphat@gmail.com>
2023-06-21 15:03:06 +0700

    add:

---
+++ sources/customer-management-jpa/src/main/java/com/codegym/customermanagementjpa/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.customermanagementjpa.repository;
+
+import com.codegym.customermanagementjpa.model.Customer;
+
+public interface CustomerRepository extends GeneralRepository<Customer> {
+
+}
---
+++ sources/customer-management-jpa/src/main/java/com/codegym/customermanagementjpa/service/GeneralService.java
@@ -0,0 +1,13 @@
+package com.codegym.customermanagementjpa.service;
+
+import java.util.List;
+
+public interface GeneralService<T> {
+    List<T> findAll();
+
+    T findById(Long id);
+
+    void save(T t);
+
+    void remove(Long id);
+}
